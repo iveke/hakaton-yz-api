1b36627113af602aaf8d742f1bfacb606d006ae6
Yevheniia <evgenia.vretik25@gmail.com>
2026-04-10 00:46:39 +0300

    add new fields(2) to Wagon class

--- Models/Wagon.cs
+++ Models/Wagon.cs
@@ -9,5 +9,12 @@ namespace hakaton_yz_api.Models
         public double Longitude { get; set; }
         public string City { get; set; } = string.Empty;
         public bool IsAvailable { get; set; }
+
+        // Цільова станція, куди вагон їде зараз.
+        public string? TargetCity { get; set; }
+
+        // Прапорець, чи знаходиться вагон у русі саме зараз.
+        // Це допоможе бекенду швидко відфільтрувати список movingWagons.
+        public bool IsMoving { get; set; }
     }
 }
