b90f6eddcf9d33ddafa211dd0ce242076fdef74b
ka-tro <brazcevska@ukr.net>
2026-04-14 22:28:27 +0300

    added geo matrix calculator

---
+++ Models/CostEdge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace hakaton_yz_api.Models
+{
+    public class CostEdge
+    {
+        public Guid WagonId { get; set; }
+        public Guid ShipmentId { get; set; }
+        public double DistanceKm { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
